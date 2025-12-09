f0310ca7fbc658450ff33814364b49abca7541ae
sumaiyyadingankar-hash <sumaiyyadingankar@gmail.com>
2025-12-09 15:03:16 +0530

    Update featured-products.js

--- assets/js/featured-products.js
+++ assets/js/featured-products.js
@@ -2,36 +2,24 @@
 const featuredProductsData = [
   {
     id: 1,
-    name: "Premium Black Abaya",
+    name: "Abaya Style dress ",
     image: "https://res.cloudinary.com/dzpjquuvs/image/upload/v1765196259/1_bq2b6k.jpg"
   },
   {
     id: 2,
-    name: "Nida Fabric Abaya",
+    name: "Denim Cotton Top",
     image: "https://res.cloudinary.com/dzpjquuvs/image/upload/v1765196260/2_ppeuvb.jpg"
   },
   {
     id: 3,
-    name: "Luxury White Khimar",
+    name: "Red & White Floral Print Tunic",
     image: "https://res.cloudinary.com/dzpjquuvs/image/upload/v1765196264/3_nzrjl3.jpg"
   },
   {
     id: 4,
-    name: "Everyday Hijab Combo",
-    image: "https://res.cloudinary.com/dzpjquuvs/image/upload/v1765196265/4_ultqkv.jpg"
-  },
-  // {
-  //   id: 5,
-  //   name: "Elegant Party Abaya",
-  //   price: "₹1999",
-  //   image: "./assets/images/features/5.jpeg"
-  // },
-  // {
-  //   id: 6,
-  //   name: "Soft Jersey Hijab",
-  //   price: "₹399",
-  //   image: "./assets/images/features/6.jpeg"
-  // }
+    name: "Pink Tunic with Sequin Sleeves",
+    image: "https://res.cloudinary.com/dzpjquuvs/image/upload/v1765262092/download_4_z8ou9g.jpg"
+  }
 ];
 
 // Render Featured Products on Home Page
@@ -55,6 +43,7 @@ displayFeaturedProducts();
 function openProductPage(id) {
   window.location.href = `./pages/product.html?id=${id}`;
 }
+
 
 
 
